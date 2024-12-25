44f11935a56ed7a3344f036e99563984285bfaeb
Kirubel Midru <kirubelmidru@gmail.com>
2024-12-25 23:02:56 +0300

    added team members

---
+++ src/lib/components/articles/Member.svelte
@@ -0,0 +1,40 @@
+<script>
+export let img;
+export let name;
+export let job;
+</script>
+
+<div>
+    <img class="post-image" src={img}/>
+    <h3>{name}</h3>
+    <p class="tag">{job}</p>
+</div>
+
+<style>
+div {
+    padding: 2rem 0;
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+    overflow: hidden;
+    transition: gap .5s;
+}
+img {
+    height: 32rem;
+    object-fit: cover;
+}
+p {
+    color: #0f6efc;
+    text-transform: uppercase;
+    letter-spacing: .2em;
+}
+h3 {
+    color: #0f6efc;
+    font-size: 28px;
+    text-transform: uppercase;
+    letter-spacing: .2em;
+}
+div:hover {
+    gap: 1rem;
+}
+</style>
--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,11 +1,6 @@
-<div class="team-wrapper">
-    <h2 style="text-align: center; font-size: 6rem;">Teams Around the Project</h2>
-</div>
+<script>
+import Team from "$lib/components/landing/Team.svelte";
+</script>
 
-<style>
-.team-wrapper {
-    height: 100vh;
-    background: black;
-    color: white;
-}
-</style>
+<Team/>
+
