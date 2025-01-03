5e28a0c5b2e58751438d3f92cc3549601ac11722
brooksolomon <officialbrooksolo@gmail.com>
2025-01-03 07:58:27 +0300

    Fix Firebase functions to correctly handle pending orders and update verified orders

--- src/routes/api/payment/callback/+server.js
+++ src/routes/api/payment/callback/+server.js
@@ -4,7 +4,7 @@ import { FireFunc } from "$lib/firebase/firebase.js";
 export async function POST({ req }) {
   var event = await FireFunc.fetchPendingOrder(req.body.trx_ref);
   console.log("recived gang")
-  FireFunc,addToVerified(event)
+  FireFunc.addToVerified(event)
   await FireFunc.deletePending(req.body.trx_ref);
   res.send(200)
 }
